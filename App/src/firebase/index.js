a6f7d530d8a76ffb71a6e6dd3a300efc8aef9227
Unknown <admin@centermatwrestling.com>
2017-10-15 19:08:52 -0400

    added credentials

--- App/src/firebase/index.js
+++ App/src/firebase/index.js
@@ -1,15 +1,20 @@
 import RNFirebase from 'react-native-firebase'
 
 const configurationOptions = {
-  debug: true
+  apiKey: "AIzaSyCcZ9CtB2xO03XzX3pCnQCSDBR_jP8_A7w",
+  authDomain: "axial-reference-542.firebaseapp.com",
+  databaseURL: "https://axial-reference-542.firebaseio.com",
+  projectId: "axial-reference-542",
+  storageBucket: "axial-reference-542.appspot.com",
+  messagingSenderId: "317768110577"
 }
 
 const firebase = RNFirebase.initializeApp(configurationOptions)
 firebase.auth().signInAnonymously()
   .then((user) => {
-    alert('Anonymous user successfully logged in')
+    alert('Anonymous user successfully logged in' + JSON.stringify(user))
   })
   .catch((err) => {
-    alert('Anonymous user signin error')
+    alert('Anonymous user signin error' + err)
   })
 export default firebase
